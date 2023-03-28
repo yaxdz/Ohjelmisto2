2e4501c75860bd794953a0ea6ba37cfcd33789bf
yasminbelhout <yasmin.belhout99@hotmail.com>
2023-03-28 17:02:49 +0300

    moduuli 12

---
+++ moduuli 12/tehtävä 2.py
@@ -0,0 +1,21 @@
+import requests
+
+
+API_KEY = "c06c76cd577a1c389c68f7fa76996594"
+
+city = input("Syötä paikkakunnan nimi: ")
+
+url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}"
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+
+    temp_kelvin = data["main"]["temp"]
+    temp_celsius = round(temp_kelvin - 273.15, 1)
+
+    weather_description = data["sää"][0]["selitys"]
+
+    print(f"Sää paikkakunnalla {city} on {weather_description}. Lämpötila on {temp_celsius} astetta Celsius-asteina.")
+else:
+    print("Sään hakeminen epäonnistui. Tarkista API-avain ja paikkakunnan nimi.")
